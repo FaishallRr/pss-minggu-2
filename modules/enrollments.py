d324ecb09bad53bd8510010394bf002f8af07b04
FaishallRr <faishalrasyid4@gmail.com>
2026-09-25 17:00:49 +0700

    Add enrollments module with GET /enrollments endpoint

---
+++ modules/enrollments.py
@@ -0,0 +1,8 @@
+enrollments = [
+    {'id': 1, 'student_id': 1, 'course_id': 1},
+    {'id': 2, 'student_id': 2, 'course_id': 1},
+    {'id': 3, 'student_id': 1, 'course_id': 2},
+]
+
+def get_enrollments():
+    return enrollments
